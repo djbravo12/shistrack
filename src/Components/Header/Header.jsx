0e4aaad1f1dd51dcd3d198fca91d315934302933
Vineet Kumar <vineetkujur12@live.com>
2025-05-08 00:35:57 +0530

    Created the Header For Mobile Viewport

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router";
+import { Bars3Icon } from '@heroicons/react/24/outline'
+import { useState } from "react";
 
 const Header = () => {
     return (
@@ -100,6 +102,16 @@ const Header = () => {
         // For authenticated users, show the Dashboard component with the routes to show the dashboard, profile, Result and  other pages.
         <header className="bg-white shadow-sm">
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        onClick={() => setMobileMenuOpen(true)}
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon aria-hidden="true" className="size-6" />
+                    </button>
+                </div>
                 <div className="flex lg:flex-1">
                     <a href="#" className="-m-1.5 p-1.5">
                         {/* <span className="sr-only text-[#8c52ff]">Shistrack</span> */}
